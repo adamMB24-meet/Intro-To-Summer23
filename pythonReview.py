7bb915243560ab80db46457c77ff200f7f584f00
adamMB24-meet <139688738+adamMB24-meet@users.noreply.github.com>
2023-07-17 08:12:06 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,3 +1,4 @@
 
 def create_youtube_video(title, description, likes, dislikes, comments):
-	youtube_video = {"title": "", "description": "", "likes": "", "dislikes": "", "comments": ""}
+	youtube_video = 
+	{"title": "I SURVIVED 100 DAYS IN MINCRAFT????!!", "description": "I SURVIVED IN MINCARFT FOR 100 DAYS, DON'T FORGET TO SUBSCRIBE AND LIKE THE VIDEO!!!", "likes": "0300", "dislikes": "100", "comments": "{}"}
